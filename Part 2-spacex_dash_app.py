2486f3467ca9118bfb8555ef4c4e96a3ad277460
NafisAnsari786 <94855409+NafisAnsari786@users.noreply.github.com>
2023-11-26 20:59:17 +0530

    Add files via upload
    Interactive visual analytics with Folium and SpaceX dash application

---
+++ Part 2-spacex_dash_app.py
@@ -0,0 +1,95 @@
+# Import required libraries
+import pandas as pd
+import dash
+from dash import html, dcc  # Update imports
+from dash.dependencies import Input, Output
+import plotly.express as px
+
+# Read the SpaceX data into a pandas dataframe
+spacex_df = pd.read_csv("spacex_launch_dash.csv")
+max_payload = spacex_df['Payload Mass (kg)'].max()
+min_payload = spacex_df['Payload Mass (kg)'].min()
+
+# Create a Dash application
+app = dash.Dash(__name__)
+
+# Create an app layout
+app.layout = html.Div(children=[
+    html.H1('SpaceX Launch Records Dashboard', style={'textAlign': 'center', 'color': '#503D36', 'font-size': 40}),
+    
+    # TASK 1: Add a dropdown list to enable Launch Site selection
+    # The default select value is for ALL sites
+    dcc.Dropdown(
+        id='site-dropdown',
+        options=[
+            {'label': 'All Sites', 'value': 'ALL'},
+            {'label': 'CCAFS LC-40', 'value': 'CCAFS LC-40'},
+            {'label': 'CCAFS SLC-40', 'value': 'CCAFS SLC-40'},
+            {'label': 'KSC LC-39A', 'value': 'KSC LC-39A'},
+            {'label': 'VAFB SLC-4E', 'value': 'VAFB SLC-4E'}
+        ],
+        value='ALL',  # Default value
+        style={'width': '80%'}
+    ),
+    
+    html.Br(),
+    
+    # TASK 2: Add a pie chart to show the total successful launches count for all sites
+    # If a specific launch site was selected, show the Success vs. Failed counts for the site
+    html.Div(dcc.Graph(id='success-pie-chart')),
+    html.Br(),
+    
+    # TASK 3: Add a slider to select payload range
+    dcc.RangeSlider(
+        id='payload-slider',
+        min=int(min_payload),  # Convert min_payload to integer
+        max=int(max_payload),  # Convert max_payload to integer
+        step=1000,
+        marks={i: str(i) for i in range(int(min_payload), int(max_payload) + 1, 1000)},  # Convert to integers
+        value=[int(min_payload), int(max_payload)]  # Convert to integers
+    ),
+    
+    # TASK 4: Add a scatter chart to show the correlation between payload and launch success
+    html.Div(dcc.Graph(id='success-payload-scatter-chart')),
+    
+])
+
+# TASK 2:
+# Add a callback function for `site-dropdown` as input, `success-pie-chart` as output
+@app.callback(
+    Output(component_id='success-pie-chart', component_property='figure'),
+    Input(component_id='site-dropdown', component_property='value')
+)
+def update_pie_chart(selected_site):
+    if selected_site == 'ALL':
+        fig = px.pie(spacex_df, names='class', title='Success vs. Failure for All Sites')
+    else:
+        filtered_df = spacex_df[spacex_df['Launch Site'] == selected_site]
+        fig = px.pie(filtered_df, names='class', title=f'Success vs. Failure for {selected_site}')
+    return fig
+
+# TASK 4:
+# Add a callback function for `site-dropdown` and `payload-slider` as inputs, `success-payload-scatter-chart` as output
+@app.callback(
+    Output(component_id='success-payload-scatter-chart', component_property='figure'),
+    [Input(component_id='site-dropdown', component_property='value'),
+     Input(component_id='payload-slider', component_property='value')]
+)
+def update_scatter_chart(selected_site, payload_range):
+    if selected_site == 'ALL':
+        filtered_df = spacex_df[(spacex_df['Payload Mass (kg)'] >= payload_range[0]) &
+                                (spacex_df['Payload Mass (kg)'] <= payload_range[1])]
+        title = 'Payload vs. Success for All Sites'
+    else:
+        filtered_df = spacex_df[(spacex_df['Launch Site'] == selected_site) &
+                                (spacex_df['Payload Mass (kg)'] >= payload_range[0]) &
+                                (spacex_df['Payload Mass (kg)'] <= payload_range[1])]
+        title = f'Payload vs. Success for {selected_site}'
+    
+    fig = px.scatter(filtered_df, x='Payload Mass (kg)', y='class', color='Booster Version Category',
+                     title=title, labels={'class': 'Launch Outcome'})
+    return fig
+
+# Run the app
+if __name__ == '__main__':
+    app.run_server(port=8060)
